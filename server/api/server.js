e3e52bb0ff24298938c6bcdc0aa2ec2ab0441991
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-08-16 00:39:38 -0500

    Added use of middlewareCofig file

--- server/api/server.js
+++ server/api/server.js
@@ -1,17 +1,10 @@
 const express = require('express');
 
-const helmet = require('helmet');
-const morgan = require('morgan');
-const cors = require('cors');
-
-//const {configureMiddleware} = require('../middlewareConfig/middlewareConfig.js');
+const { configureMiddleware } = require('../middlewareConfig/middlewareConfig.js');
 
 const server = express();
 
-server.use(helmet());
-server.use(morgan());
-server.use(cors());
-server.use(express.json());
+configureMiddleware(server);
 
 module.exports = {
   server
