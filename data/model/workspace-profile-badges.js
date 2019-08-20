4a076fb2e66978789caf11f1fa6214676c98fa65
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-08-19 22:22:06 -0500

    Completed workspace profiles badges data model

--- data/model/workspace-profile-badges.js
+++ data/model/workspace-profile-badges.js
@@ -1,1 +1,36 @@
 const db = require('../dbConfig');
+
+module.exports = {
+  find,
+  findById,
+  insert,
+  update,
+  remove,
+};
+
+function find(){
+  return db('WORKSPACE_PROFILES_BADGES');
+};
+
+function findById(id){
+  return db('WORKSPACE_PROFILES_BADGES')
+    .where({ id });
+};
+
+function insert(badge){
+  return db('WORKSPACE_PROFILES_BADGES')
+    .insert(badge)
+    .first();
+};
+
+function update(id, badge){
+  return db('WORKSPACE_PROFILES_BADGES')
+    .where({ id })
+    .update(badge);
+};
+
+function remove(id){
+  return db('WORKSPACE_PROFILES_BADGES')
+    .where({ id })
+    .del();
+};
