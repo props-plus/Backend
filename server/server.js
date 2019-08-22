883a60923ae0ef934885e83605e197de7ba57d3a
justinandrade <justinandrade@github.com>
2019-08-22 19:23:28 -0400

    verifyToken created, private route added

--- server/server.js
+++ server/server.js
@@ -1,24 +1,26 @@
-const express = require('express')
-const server = express()
-const helmet = require('helmet')
-const morgan = require('morgan')
-const cors = require('cors')
-const oauth = require('./routes/oauth')
+const express = require("express");
+const server = express();
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const oauth = require("./routes/oauth");
+const authzero = require("./auth/authzero");
 
-server.use(express.json())
-server.use(helmet())
-server.use(morgan('dev'))
-server.use(cors())
-server.use('/oauth', oauth)
+server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
+server.use(cors());
+server.use("/oauth", oauth);
+server.use("/protected", authzero);
 // server.use('/props', props)
 
-server.post('/props', (req, res) => {
-    console.log(req.query)
-    res.json(req)
-})
+server.post("/props", (req, res) => {
+  console.log(req.query);
+  res.json(req);
+});
 
-server.get('/', (req, res) => {
-    res.json({ message: 'You have reached the api' })
-})
+server.get("/", (req, res) => {
+  res.json({ message: "You have reached the api" });
+});
 
-module.exports = server
+module.exports = server;
