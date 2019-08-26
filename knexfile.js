f70b95d6912af998ae08748e6778a234d8f33398
Scott Grobe <gitgrobe@gmail.com>
2019-08-25 21:53:39 -0400

    basic backend testing

--- knexfile.js
+++ knexfile.js
@@ -1,27 +1,40 @@
 // Update with your config settings.
-require('dotenv').config()
-const devConnection = process.env.DEV_DB
-const stage1 = process.env.STAGE1
-const stage2 = process.env.STAGE2
-const prod = process.env.PROD
-const pg = require('pg')
-pg.defaults.ssl = true
+require("dotenv").config();
+const devConnection = process.env.DEV_DB;
+const stage1 = process.env.STAGE1;
+const stage2 = process.env.STAGE2;
+const prod = process.env.PROD;
+const pg = require("pg");
+pg.defaults.ssl = true;
 
 module.exports = {
-    development: {
-        client: 'pg',
-        connection: process.env.DATABASE_URL || devConnection,
-        pool: {
-            min: 2,
-            max: 10
-        },
-        useNullAsDefault: true,
-        migrations: {
-            tableName: 'knex_migrations',
-            directory: './data/migrations'
-        },
-        seeds: {
-            directory: './data/seeds'
-        }
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL || devConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    useNullAsDefault: true,
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
-}
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+};
