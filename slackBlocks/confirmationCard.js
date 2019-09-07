9b8870153828c145021a9edc9f55198156cba55d
iner Lofgren <iner.lofgren@gmail.com>
2019-09-06 18:34:18 -0700

    Created Send Self Card

--- slackBlocks/confirmationCard.js
+++ slackBlocks/confirmationCard.js
@@ -1,23 +1,39 @@
 const confirmationCard = (reciever, status) => {
-    const message = status == "Cancel" ? ":x: props have been canceled" : ":heavy_check_mark: props have been sent"
+    let header
+    let message
+    switch (status) {
+        case 'Cancel':
+            header = 'Your prop request has been canceled'
+            message = 'No props have been sent'
+            break
+        case 'Same':
+            header = "You can't send props to yourself"
+            message =
+                "Silly :rabbit:, tricks are for kids.You can't give yourself props!"
+            break
+        default:
+            header = `Props to ${reciever} have been sent`
+            message = ':heavy_check_mark: props have been sent'
+    }
+
     return [
         {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": `Props to ${reciever} have been sent`
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: header
             }
         },
         {
-            "type": "section",
-            "fields": [
+            type: 'section',
+            fields: [
                 {
-                    "type": "mrkdwn",
-                    "text": message
+                    type: 'mrkdwn',
+                    text: message
                 }
             ]
         }
     ]
 }
 
-module.exports = confirmationCard;
+module.exports = confirmationCard
