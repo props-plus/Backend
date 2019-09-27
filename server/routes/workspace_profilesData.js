364a97469aeb35fd576741c84fbe4f1b98c76980
justinandrade <j.andrade.john@gmail.com>
2019-09-26 22:26:17 -0400

    team info endpoint created

--- server/routes/workspace_profilesData.js
+++ server/routes/workspace_profilesData.js
@@ -21,4 +21,18 @@ router.get('/user', SPAAuth, async (req, res) => {
     }
 })
 
+// Displays the workspace team data
+router.get('/team', SPAAuth, async (req, res) => {
+    try {
+        const teamInfo = await db.find();
+        if (teamInfo.fk_workspace_id === req.teamInfo.fk_workspace_id) { // matching the current users workspace ID with the workspace table and returning results
+            res.status(200).json(teamInfo)
+        } else {
+            res.status(404).json({ message: 'There was an error retrieving the team.' })
+        }
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
+})
+
 module.exports = router
