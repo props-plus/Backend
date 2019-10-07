50f1a22e4fd42b4fb7f25fd9325c1f536b4b985b
Lambda_School_Loaner_202 <lambda_school_loaner_202@Lambda-School-Loaner-202s-MacBook-Pro.local>
2019-10-07 01:37:24 -0400

    Cleaned userview, as well as new routes and models

--- server/routes/userView.js
+++ server/routes/userView.js
@@ -1,12 +1,28 @@
 const db = require('../../data/model/props')
 const SPAAuth = require('../../middleware/SPAauth');
+const wpd = require('../../data/model/workpace_profiles')
 
 const express = require('express');
 const router = express.Router();
 
 
-// Route that returns the props a user has received
+ // Route for user profile
+ router.get('/', SPAAuth, async (req, res) => {
+    try {
+        const userInfo = await wpd.findByUserName(req.userInfo.userName)
+        res.status(200).json(userInfo)
+        if (userInfo) {
+        } else {
+            res.status(404).json({ message: 'Invalid User' })
+        }
 
+    } catch (error) {
+        res.status(500).json({ message: error })
+
+    }
+}) 
+
+// Route that returns the props a user has received
 router.get('/received', SPAAuth, async (req, res) => {
     try {
         const userProps = await db.findByPropsReceived(req.userInfo.id)
