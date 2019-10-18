0a2e6b10e5316d1991d6897df8ef73af307bbb71
Lambda_School_Loaner_202 <lambda_school_loaner_202@Lambda-School-Loaner-202s-MacBook-Pro.local>
2019-10-18 03:41:31 -0400

    fixed token so correct value is being stored in session storage

--- server/routes/oauth.js
+++ server/routes/oauth.js
@@ -1,35 +1,39 @@
-const express = require('express')
-const request = require('request')
-const userInfo = require('../../helpers/userinfo')
-const router = express.Router()
-const client_id = process.env.CLIENT_ID
-const client_secret = process.env.CLIENT_SECRET
+const express = require("express");
+const request = require("request");
+const userInfo = require("../../helpers/userinfo");
+const router = express.Router();
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
 
-router.get('/', async (req, res) => {
-    if (!req.query.code) {
-        res.status(500).json({ error: 'Not getting code' })
-    } else {
-        request(
-            {
-                url: 'https://slack.com/api/oauth.access',
-                qs: {
-                    code: req.query.code,
-                    client_id,
-                    client_secret
-                },
-                method: 'GET'
-            },
-            async (error, resp, body) => {
-                if (error) {
-                    console.error(error)
-                } else {
-                    body = JSON.parse(body)
-                    const user = await userInfo(body.access_token, body.user_id)
-                    res.json({ token: user.uri.query.split('token=')[1] })
-                }
-            }
-        )
-    }
-})
+router.get("/", async (req, res) => {
+  if (!req.query.code) {
+    res.status(500).json({ error: "Not getting code" });
+  } else {
+    request(
+      {
+        url: "https://slack.com/api/oauth.access",
+        qs: {
+          code: req.query.code,
+          client_id,
+          client_secret
+        },
+        method: "GET"
+      },
+      async (error, resp, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          body = JSON.parse(body);
+          const user = await userInfo(body.access_token, body.user_id);
+          let myToken = user.uri.query
+            .split("&user")
+            .shift()
+            .replace(/token=/, "");
+          res.json({ token: myToken });
+        }
+      }
+    );
+  }
+});
 
-module.exports = router
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,40 +1,40 @@
-const express = require('express')
-const server = express()
-const helmet = require('helmet')
-const morgan = require('morgan')
-const cors = require('cors')
-const props = require('./routes/props')
-const button = require('./routes/slackActions')
+const express = require("express");
+const server = express();
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const props = require("./routes/props");
+const button = require("./routes/slackActions");
 //const test = require('./routes/slackActionsTest')
-const anonProps = require('./routes/anonProps')
+const anonProps = require("./routes/anonProps");
 //const recentProps = require('./routes/recentProps')
-const info = require('./routes/info')
-const oauth = require('./routes/oauth')
-const authZero = require('./auth/authZero')
-const authError = require('./auth/authError')
-const SPAAuth = require('../middleware/SPAauth')
-const userView = require('./routes/userView')
+const info = require("./routes/info");
+const oauth = require("./routes/oauth");
+const authZero = require("./auth/authZero");
+const authError = require("./auth/authError");
+const SPAAuth = require("../middleware/SPAauth");
+const userView = require("./routes/userView");
 
-server.use(express.json())
-server.use(helmet())
-server.use(morgan('dev'))
-server.use(cors())
+server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
+server.use(cors());
 
-server.use('/props', props)
-server.use('/anon-props', anonProps)
-server.use('/slackActions', button)
+server.use("/props", props);
+server.use("/anon-props", anonProps);
+server.use("/slackActions", button);
 //server.use('/slackActionsTest', test)
 //server.use('/recent-props', recentProps)
-server.use('/info', info)
-server.use('/oauth', oauth)
-server.use('/protected', authZero)
-server.use('/user-data', userView)
+server.use("/info", info);
+server.use("/oauth", oauth);
+server.use("/protected", authZero);
+server.use("/user-data", userView);
 
 // Error handling for unauthorized users!
-server.use(authError)
+server.use(authError);
 
-server.get('/', SPAAuth, (req, res) => {
-    res.json({ message: 'You have reached the api' })
-})
+server.get("/", SPAAuth, (req, res) => {
+  res.json({ message: "You have reached the api" });
+});
 
-module.exports = server
+module.exports = server;
